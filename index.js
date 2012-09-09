c97593041e81e34d06ff903e4f109a6ae8f165ed
Valeri Bogdanov <valeribogdanov@gmail.com>
2012-09-09 21:09:08 +0300

    Initial tunnels tests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
 var files = fs.readdirSync(__dirname+"/lib");
 for(var i = 0; i<files.length; i++)
-  module.exports[files[i].replace(".js", "")] = require("./lib/"+files[i]);
+  if (files[i].indexOf(".js") !== -1)
+    module.exports[files[i].replace(".js", "")] = require("./lib/"+files[i]);
