49b007ea504513e7e6ea610dfcbd039fb0019c4c
Boris Filipov <obiwonn@gmail.com>
2013-01-15 22:35:09 +0200

    fixed release script for dev-master branches development

--- scripts/release.js
+++ scripts/release.js
@@ -6,11 +6,6 @@ newVersion[2] = (parseInt(newVersion[2])+1).toString();
 newVersion = newVersion.join(".");
 cd(__dirname+"/../");
 
-if(exec("git pull --ff origin master").code != 0){
-  echo("Error: failed to pull origin master");
-  exit(1);
-}
-
 // TODO find out how to trap errors from sed bellow
 sed('-i', '"version": "'+p.version+'"', '"version": "'+newVersion+'"', "package.json");
 
@@ -24,12 +19,33 @@ if(exec("git commit -am '"+newVersion+" release'").code != 0){
   exit(1);
 }
 
+if(exec("git checkout master").code != 0){
+  echo("Error: failed to checkout master");
+  exit(1);
+}
+
+if(exec("git pull --ff origin master").code != 0){
+  echo("Error: failed to pull origin master");
+  exit(1);
+}
+
+if(exec("git merge develop").code != 0){
+  echo("Error: failed to merge develop");
+  exit(1);
+}
+
+if(exec("git push").code != 0){
+  echo("Error: failed to git push");
+  exit(1);
+}
+
 if(exec("npm publish").code != 0){
   echo("Error: failed to npm publish");
   exit(1);
 }
 
-if(exec("git push origin master").code != 0){
-  echo("Error: failed to git push");
+if(exec("git checkout develop").code != 0){
+  echo("Error: failed to checkout develop");
   exit(1);
 }
+
