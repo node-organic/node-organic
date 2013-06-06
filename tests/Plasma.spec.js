885ba4e8c6c6550963cfea6394f6158ec0461cff
Valeri Bogdanov <valeribogdanov@gmail.com>
2013-06-06 18:50:07 +0300

    added test for the last bug

--- tests/Plasma.spec.js
+++ tests/Plasma.spec.js
@@ -38,7 +38,7 @@ describe("Plasma", function(){
     expect(c).toBe(1);
   });
 
-  it("should unregister listerner for chemical", function(){
+  it("should unregister listener for chemical", function(){
     var c = 0;
     var m = function(){
       c += 1;
@@ -48,5 +48,12 @@ describe("Plasma", function(){
     plasma.off("test3", m);
     plasma.emit(new Chemical("test3"));
     expect(c).toBe(1);
+  });
+  
+  it("should not throw exception if no listeners are registered for a given chemical", function(){
+    expect(function () {
+      var plasma2 = new Plasma();
+      plasma2.emit(new Chemical("Test 4"));
+    }).not.toThrow(); 
   });
 });
