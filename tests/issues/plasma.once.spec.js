04d19e5f10c231afae11a6e651b6cf4482d4c5c7
Boris Filipov <obiwonn@gmail.com>
2013-10-15 17:14:38 +0300

    rewritten issue spec using beforeEach for setup of the issue case

--- tests/issues/plasma.once.spec.js
+++ tests/issues/plasma.once.spec.js
@@ -4,24 +4,21 @@ describe("issue test case", function(){
   var plasma = new Plasma()
   var invoked = 0
 
-  it("when attaching once and on handlers on plasma", function(){
+  beforeEach(function () {
     plasma.once("test", function(){
       invoked += 1
       return false
-    })
+    });
     plasma.on("test", function(){
       invoked += 1
-    })
+    });
+  });
 
-    describe("should invoke", function(){
-      it("'once' and 'on' handlers on test chemical", function(){
-        plasma.emit("test")
-        expect(invoked).toBe(2)
-      })
-      it("only 'on' handler on second test chemical", function(){
-        plasma.emit("test")
-        expect(invoked).toBe(3)
-      })
-    })
+  it("invokes 'once' once and on every time", function(){
+    plasma.emit("test")
+    expect(invoked).toBe(2)
+    plasma.emit("test")
+    expect(invoked).toBe(3)
   })
+
 })
